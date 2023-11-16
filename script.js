ceadc1d3a1398c00f95603e2486db3d61fcf61fe
Camila Blanco <cami.blanco@hotmail.com>
2023-11-15 21:05:12 -0300

    ultimo guardado

---
+++ script.js
@@ -0,0 +1,8 @@
+let miBoton = document.getElementById("boton");
+
+miBoton.addEventListener("mouseover", function(){
+    document.body.style.backgroundColor = "black";
+})
+miBoton.addEventListener("mouseout", function(){
+    document.body.style.backgroundColor = "white";
+})

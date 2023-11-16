147bfbb44c503963339ad01e77d3d75195881395
Camila Blanco <cami.blanco@hotmail.com>
2023-11-16 16:20:11 -0300

    nuevo guardado

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+
+let nombre = prompt ("¿Cuál es tu nombre?");
+let alerta = alert("bienvenido" + "" + nombre);
+
+
+//FOOTTER
+
+const fecha = document.querySelector('.fecha');
+function fechaActual(){
+    let fechaHoy = new Date().getFullYear();
+    fecha.textContent = fechaHoy;
+}
+document.addEventListener('DOMContentLoaded',()=>{
+    fechaActual();
+});
+
+
 let miBoton = document.getElementById("boton");
 
-miBoton.addEventListener("mouseover", function(){
-    document.body.style.backgroundColor = "black";
-})
-miBoton.addEventListener("mouseout", function(){
-    document.body.style.backgroundColor = "white";
-})
+miBoton.addEventListener("click", miFuncion);
+
+function miFuncion(){
+    document.body.style.backgroundColor = "hsla(112, 81%, 19%, 0.7)"
+}
